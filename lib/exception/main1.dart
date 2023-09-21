19bfe12eaec9a881266bdd24d881d406144139da
sbux1992 <sinsunkangg@naver.com>
2023-09-21 17:42:32 +0900

    업데이트

---
+++ lib/exception/main1.dart
@@ -0,0 +1,17 @@
+void main() {
+  Exception exception;
+  Error error;
+  try {
+    int result = 10 ~/ 0;
+    print("result : $result");
+  } catch (error, stackTrace) {
+    // 예외 처리 코드
+    print("An error : $error");
+    print("A stackTrace : $stackTrace");
+  } finally {
+    // 예외 발생 여부와 상관 없이 항상 실행 되는 코드
+    print("반드시 실행 되는 블록");
+  }
+
+  print("--------------------------");
+}
